40fc063f4c56935cf22bdd35fd4d4558ee3767bf
Merry <Meryem.akk@outlook.com>
2025-03-20 23:06:33 +0100

    User can upload own dataset

--- R/server.R
+++ R/server.R
@@ -1,15 +1,94 @@
 # Define server logic required to draw the plots####################################################################################
-server <- function(input,output){
+server <- function(input,output, session){
   ### Welcome page ###################################################################################################################
-  output$table <- renderTable({
-    head(diabetes, input$rows)
-  }) # End output table
-  bs_themer() # Interactive tool to chose personal theme
+  # Data reactive expression
+  data <- reactive({
+    req(input$upload)  # Ensure a file is uploaded
+    
+    # Get file extension
+    ext <- tools::file_ext(input$upload$name)
+    
+    # Read the file based on its extension
+    if (ext == "csv") {
+      df <- read.csv(input$upload$datapath, stringsAsFactors = TRUE)
+    } else if (ext %in% c("xls", "xlsx")) {
+      if (!requireNamespace("readxl", quietly = TRUE)) {
+        install.packages("readxl")
+        library(readxl)
+      }
+      df <- readxl::read_excel(input$upload$datapath) %>% as.data.frame()
+    } else if (ext %in% c("tsv", "txt")) {
+      df <- read.delim(input$upload$datapath, stringsAsFactors = TRUE)
+    } else {
+      stop("Unsupported file format")
+    }
+    
+    # Convert character columns to factors if they have few unique values
+    for (col in names(df)) {
+      if (is.character(df[[col]]) && length(unique(df[[col]])) <= 50) {
+        df[[col]] <- as.factor(df[[col]])
+      }
+    }
+    
+    return(df)
+  })
+  
+  # Data separation functions
+  categorical <- reactive({
+    req(data())
+    df <- data()
+    df[, sapply(df, function(x) is.factor(x) | is.character(x)), drop = FALSE]
+  })
+  
+  numerical <- reactive({
+    req(data())
+    df <- data()
+    df[, sapply(df, is.numeric), drop = FALSE]
+  })
+  
+  # Display data preview
+  output$files <- renderTable({
+    req(data())
+    head(data(), 10)
+  })
+  
+  # Update inputs when data changes
+  observe({
+    req(data())
+    
+    # Get column names
+    num_cols <- names(numerical())
+    cat_cols <- names(categorical())
+    all_cols <- names(data())
+    
+    # Update single variable inputs
+    if (length(num_cols) > 0) {
+      updateSelectInput(session, "single_var_num", choices = num_cols, selected = num_cols[1])
+    }
+    
+    if (length(cat_cols) > 0) {
+      updateSelectInput(session, "single_var_cat", choices = cat_cols, selected = cat_cols[1])
+    }
+    
+    # Update two-variable inputs
+    updateSelectInput(session, "x_var", choices = all_cols, 
+                      selected = if(length(num_cols) > 0) num_cols[1] else all_cols[1])
+    updateSelectInput(session, "y_var", choices = all_cols, 
+                      selected = if(length(num_cols) > 1) num_cols[2] else all_cols[min(2, length(all_cols))])
+    
+    # Update multi-variable inputs
+    updateSelectInput(session, "x_multi", choices = all_cols, 
+                      selected = if(length(num_cols) > 0) num_cols[1] else all_cols[1])
+    updateSelectInput(session, "y_multi", choices = all_cols, 
+                      selected = if(length(num_cols) > 1) num_cols[2] else all_cols[min(2, length(all_cols))])
+    updateSelectInput(session, "third_var", choices = all_cols, 
+                      selected = if(length(num_cols) > 2) num_cols[3] else all_cols[min(3, length(all_cols))])
+  })
   ### 1 variable plots ###############################################################################################################
   output$single_var_num_plot <- renderPlot({
+    plotData1 <- ggplot(data = data(), mapping = aes_string(input$single_var_num))
     if (input$num_plot_type == "Histogram") {
-      ggplot(data = diabetes,
-             mapping = aes_string(input$single_var_num)) +
+      plotData1 +
         geom_histogram(bins = input$bins , fill = input$color1 , color = "black") +
         theme_bw() +
         ggtitle(paste0("Histogram of ", input$single_var_num)) +
@@ -17,16 +96,14 @@ server <- function(input,output){
               panel.grid.major = element_blank(), 
               panel.grid.minor = element_blank())
     } else if (input$num_plot_type == "Boxplot") {
-      ggplot(data = diabetes,
-             mapping = aes_string(y = input$single_var_num)) +
+      plotData1 +
         geom_boxplot(fill = input$color1, color = "black") +
         theme_bw() +
         ggtitle(paste0("Boxplot of ", input$single_var_num)) +
         theme(plot.title = element_text(hjust = 0.5, face = "bold")) +
         coord_flip()
     } else if (input$num_plot_type == "Density Plot") {
-      ggplot(data = diabetes,
-             mapping = aes_string(input$single_var_num)) +
+      plotData1 +
         geom_density(fill = input$color1, alpha = 0.5) +
         ggtitle(paste0("Density plot of ", input$single_var_num)) +
         theme_bw() +
@@ -38,7 +115,7 @@ server <- function(input,output){
   
   output$single_var_cat_plot <- renderPlot({
     if (input$cat_plot_type == "Pie Chart"){
-      slices <- table(diabetes[[input$single_var_cat]])
+      slices <- table(data()[[input$single_var_cat]])
       pie(slices,
           main = paste0("Pie Chart of ", input$single_var_cat),
           col = rainbow(length(slices))
@@ -47,7 +124,7 @@ server <- function(input,output){
   })# End output single_var_cat_plot 
   ### 2 variable plots ##############################################################################################################
   output$two_var_plot <- renderPlot({
-    plotData2 <- ggplot(data = diabetes, mapping = aes_string(x = input$x_var, y = input$y_var))
+    plotData2 <- ggplot(data = data(), mapping = aes_string(x = input$x_var, y = input$y_var))
     if (input$plot_type2 == "Scatterplot") {
       scatplot <- plotData2 +
         geom_point(color = input$color2, alpha = 0.6) +
@@ -62,7 +139,7 @@ server <- function(input,output){
       } # End if-loop
     } else if (input$plot_type2 =="Scatterplot with Boxplots") {
       # Scatterplot
-      scatter_plot <- ggplot(data = diabetes, mapping = aes_string(x = input$x_var, y = input$y_var)) +
+      scatter_plot <- plotData2 +
         geom_point(color = input$color2, alpha = 0.6) +
         ggtitle(paste0("Scatterplot of ", input$x_var, " vs. ", input$y_var)) +
         theme_bw() + 
@@ -73,14 +150,14 @@ server <- function(input,output){
           geom_smooth(method = "lm", color = "red", linewidth = 2)
       } # End if-loop
       # X-axis boxplot
-      x_boxplot <- ggplot(data = diabetes, mapping = aes_string(x = input$x_var)) +
+      x_boxplot <- ggplot(data = data(), mapping = aes_string(x = input$x_var)) +
         geom_boxplot(fill = input$color2, color = "black") +
         theme_bw()
       theme(plot.title = element_text(hjust=0.5, face = "bold"), 
             panel.grid.major = element_blank(), 
             panel.grid.minor = element_blank())
       # Y-axis boxplot
-      y_boxplot <- ggplot(data = diabetes, mapping = aes_string(x = input$y_var)) +
+      y_boxplot <- ggplot(data = data(), mapping = aes_string(x = input$y_var)) +
         geom_boxplot(fill = input$color2, color = "black") +
         coord_flip() +
         theme_bw()
@@ -109,15 +186,13 @@ server <- function(input,output){
   ### Multiple variable plots ########################################################################################################
   output$multi_var_plot <- renderPlot({
     if (input$plot_type3 == "Bubble Chart"){
-      ggplot(data = diabetes,
-             mapping = aes_string(x = input$x_multi, y = input$y_multi, size = input$third_var)) +
+      ggplot(data = data(), mapping = aes_string(x = input$x_multi, y = input$y_multi, size = input$third_var)) +
         geom_point(color = input$color3) +
         ggtitle(paste0("Bubble chart of ", input$x_multi, " vs. ", input$y_multi, " with bubble size of ", input$third_var)) +
         theme_bw() +
         theme(plot.title=element_text(hjust=0.5, face = "bold"))
     } else if (input$plot_type3 == "Heatmap") {
-      ggplot(data = diabetes, 
-             mapping = aes_string(x = input$x_multi, y = input$y_multi, fill = input$third_var)) + 
+      ggplot(data = data(), mapping = aes_string(x = input$x_multi, y = input$y_multi, fill = input$third_var)) + 
         geom_tile() +
         ggtitle(paste0("Heatmap: Relationship Between ", input$x_multi, " and ", input$y_multi, " with ", input$third_var)) +
         theme_bw() +
--- R/ui.R
+++ R/ui.R
@@ -7,145 +7,82 @@ library(gridExtra)             # Create plot grids for ggplot
 library(corrplot)              # Correlation plot maker
 library(dplyr)                 # Used to select numerical variables only for corplot
 
-# Import dataset#####################################################################################################################
-df <- read.csv("xxx.csv", header = TRUE)
-
-# Pre-processing data ###############################################################################################################
-#Devide numerical and categorical variables
-categorical <- diabetes[sapply(diabetes, function(x) is.factor(x) | is.character(x))]
-numerical <- diabetes[, sapply(diabetes, is.numeric)]
-
 # Define UI for application that draws the plots####################################################################################
 ui <- navbarPage(
-  "BIT08 Assignment",
-  theme = bs_theme(),
+  "Graph Designer",
   tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
   #Welcome tab####################################################################################################################
   tabPanel("Welcome",
            sidebarLayout(
-             sidebarPanel(img(src = "funny.png")),
+             sidebarPanel(
+               fileInput("upload", "Upload Data File", buttonLabel = "Browse...",
+                         accept = c(".csv", ".tsv", ".txt", ".xls"),
+                         multiple = FALSE),
+               tableOutput("files")
+               ), # End sidebarPanel
              mainPanel(
-               HTML(r"(
-                <h2>Welcome to the BIT08 Assignment Shiny App</h2>
-                <p>
-                    This interactive application was developed for the BIT08 assignment to explore and visualize a diabetes dataset.
-                    It allows users to generate insightful plots and examine relationships between various variables. 
-                    The dataset includes clinical measurements such as glucose levels, BMI, insulin levels, and more.
-                </p>
-                <h3>Objective</h3>
-                <p>
-                    The primary goal of this app is to help users understand patterns within the dataset through visual exploration.
-                    This includes:
-                    <ul>
-                        <li>Single-variable distributions</li>
-                        <li>Relationships between two variables</li>
-                        <li>Multi-variable interactions</li>
-                    </ul>
-                </p>
-                <h3>Dataset Overview</h3>
-                <p>
-                    The dataset contains the following key variables:
-                    <ul>
-                        <li><b>Pregnancies</b> - Number of times a patient has been pregnant</li>
-                        <li><b>Glucose</b> - Blood glucose concentration </li>
-                        <li><b>Blood Pressure</b> - Blood pressure </li>
-                        <li><b>Skin Thickness</b> - Skin Tickness </li>
-                        <li><b>Insulin</b> - Insulin </li>
-                        <li><b>BMI</b> - Body Mass Index </li>
-                        <li><b>Diabetes Pedigree Function</b> - Likelihood of diabetes based on family history </li>
-                        <li><b>Age</b> - Age in years </li>
-                        <li><b>Outcome</b> - Diabetes diagnosis represented as a binary value (1 = Patient has diabetes, 0 = Patient does not have diabetes) </li>
-                        <li><b>Pregnancy_Category</b> - Pregnancies devided in 5 groups:</li>
-                            <ul>
-                              <li>Nulliparous: 0 pregnancies</li>
-                              <li>Low Parity: 0-3 pregnancies</li>
-                              <li>Moderate Parity: 3-6 pregnancies</li>
-                              <li>High Parity: 6-10 pregnancies</li>
-                              <li>Grand Multiparity: 10-17 pregnancies</li>
-                            </ul>
-                        <li><b>BMI_Category</b> - </li>
-                            <ul>
-                              <li>Underweight: BMI < 18.5</li>
-                              <li>Normal weight: BMI between 18.5 and 24.9</li>
-                              <li>Overweight: BMI between 25 and 29.9</li>
-                              <li>Obese: BMI > 30</li>
-                            </ul>
-                        <li><b>Age_Category</b> - </li>
-                            <ul>
-                              <li>Young Adult: 20-35 years old</li>
-                              <li>Middle-aged Adult: 35-50 years old</li>
-                              <li>Mature Adult: 50-65 years old</li>
-                              <li>Senior: 65-81 years old</li>
-                            </ul>
-                        <li><b>Outcome2</b> - Diabetes Diagnosis (Yes = Patient has diabetes, No = Patient does not have diabetes)</li>
-                    </ul>
-                </p>
-                <p>
-                    A few categorical variables were manually created for better visualization. Unusual values, such as a BMI of 0, 
-                    were removed before designing the Shiny app. These modifications can be found within the app folder as "pre-procesisingDiabetes.R".
-                </p>
-            )"), # End of HTML
-            numericInput(inputId = "rows",
-                         label = "Number of rows to display:",
-                         value = 10,
-                         min = 1,
-                         max = nrow(diabetes)),
-            tableOutput("table")
+               h3("Welcome to Graph Designer"),
+               p("Upload your dataset to get started with data visualization."),
+               p("This tool supports CSV, TSV, TXT, and Excel files."),
+               tableOutput("files")
              ) # End mainPanel
            ) # End sidebarLayout
   ), # End Welcome tab
   #Single-var tab##################################################################################################################
   tabPanel("Single-variable plots", # Tab for Single-variable plots
-           layout_columns(
+           fluidPage(
              #Upper section
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput(inputId = "single_var_num", 
-                             label = "Choose a numerical variable:", 
-                             choices = names(numerical), 
-                             selected = "Glucose"), # End input
-                 sliderInput(inputId = "bins",
-                             label = "Number of bins",
-                             min = 1, 
-                             max = 50, 
-                             value = 30), # End slider
-                 colourInput(inputId = "color1",
-                             label = "Choose color:",
-                             value = "#FFC0CB"), # End color
-                 radioButtons(inputId = "num_plot_type", 
-                              label = "Choose plot type:",
-                              choices = c("Histogram", "Boxplot", "Density Plot"),
-                              selected = "Histogram"),  # End radio button
-               ), # End sidebar panel
-               mainPanel(
-                 h3("Visualizing a Single numerical variable"),
-                 HTML("<p>This section allows you to explore the distribution of a single numerical variable. 
-                  You can generate histograms, boxplots or density plots. 
-                  Adjust the number of bins for histograms and choose custom colors.</p>"),
-                 plotOutput("single_var_num_plot")
-               ) # End mainpanel
-             ), # End sidebarlayout
+             fluidRow(
+               column(4,
+                wellPanel(
+                   selectInput(inputId = "single_var_num", 
+                               label = "Choose a numerical variable:", 
+                               choices = NULL, 
+                               selected = "Glucose"),
+                   sliderInput(inputId = "bins",
+                               label = "Number of bins",
+                               min = 1, 
+                               max = 50, 
+                               value = 30),
+                   colourInput(inputId = "color1",
+                               label = "Choose color:",
+                               value = "#FFC0CB"),
+                   radioButtons(inputId = "num_plot_type", 
+                                label = "Choose plot type:",
+                                choices = c("Histogram", "Boxplot", "Density Plot"),
+                                selected = "Histogram")
+                ) # End Wellpanel
+               ), # End column
+               column(8,
+                      h3("Visualizing a Single numerical variable"),
+                      p("This section allows you to explore the distribution of a single numerical variable. 
+                   You can generate histograms, boxplots or density plots. 
+                   Adjust the number of bins for histograms and choose custom colors."),
+                      plotOutput("single_var_num_plot", height = "400px")
+               ) # End column
+             ), # End fluidRow
              
              # Lower section
-             sidebarLayout(
-               sidebarPanel(
-                 selectInput(inputId = "single_var_cat", 
-                             label = "Choose a categorical variable:", 
-                             choices = names(categorical), 
-                             selected = "Age_Category"), # End input
-                 radioButtons(inputId = "cat_plot_type", 
-                              label = "Choose plot type:",
-                              choices = c("Pie Chart"),
-                              selected = "Pie Chart"),  # End radio button
-               ), # End sidebar panel
-               mainPanel(
-                 h3("Visualizing a Single categorical variable"),
-                 HTML("<p>For the categorical variables, a pie chart is available to show the proportion of each category in the dataset.</p>"),
-                 plotOutput("single_var_cat_plot")
-               ) # End mainpanel
-             ), # End sidebarlayout
-             col_widths = c(12,12)
-           ), # End layout columns
+             fluidRow(
+               column(4,
+                wellPanel(
+                   selectInput(inputId = "single_var_cat", 
+                               label = "Choose a categorical variable:", 
+                               choices = NULL, 
+                               selected = "Age_Category"),
+                   radioButtons(inputId = "cat_plot_type", 
+                                label = "Choose plot type:",
+                                choices = c("Pie Chart"),
+                                selected = "Pie Chart")
+                ), # End wellPanel
+               ), # End column
+               column(8,
+                      h3("Visualizing a Single categorical variable"),
+                      p("For categorical variables, pie charts and bar charts are available to show the proportion of each category in the dataset."),
+                      plotOutput("single_var_cat_plot", height = "400px")
+               ) # End column
+             ), # End fluidRow
+           ) # End fluidpage
   ), # End tabpanel Single-variable plots
   #two-var tab##################################################################################################################
   tabPanel("Two-variable plots", # Tab for Two-variable plots
@@ -153,11 +90,11 @@ ui <- navbarPage(
              sidebarPanel(
                selectInput(inputId = "x_var",
                            label =  "X-axis variable:",
-                           choices = names(diabetes),
+                           choices = NULL,
                            selected = "Glucose"), # End input 1
                selectInput(inputId = "y_var",
                            label =  "Y-axis variable:",
-                           choices = names(diabetes),
+                           choices = NULL,
                            selected = "BMI"), # End input 2
                colourInput(inputId = "color2",
                            label = "Choose color:",
@@ -171,16 +108,15 @@ ui <- navbarPage(
                              value = TRUE)
              ), # End sidebarPanel
              
-             mainPanel(h3("Visualizing two variables"),
-                       HTML(r"(<p>This section allows you to analyze relationships between two variables.
-                             <ul>
-                                  <li>Scatterplots help visualize trends between two numerical variables. You can optionally add a regression line to see potential correlations.</li>
-                                  <li>Violin plots provide insights into the distribution of a numerical variable across different categories.</li>
-                                  <li>Line charts are useful for analyzing trends over time or ordered data.</li>
-                            </ul>
-                            </p>
-                            )"),
-                       plotOutput("two_var_plot"),
+             mainPanel(
+               h3("Visualizing two variables"),
+               p("This section allows you to analyze relationships between two variables."),
+               tags$ul(
+                 tags$li("Scatterplots help visualize trends between two numerical variables. You can optionally add a regression line to see potential correlations."),
+                 tags$li("Violin plots provide insights into the distribution of a numerical variable across different categories."),
+                 tags$li("Line charts are useful for analyzing trends over time or ordered data.")
+               ),
+               plotOutput("two_var_plot", height = "500px")
              )# End mainpanel
            )# End sidebarlayout
   ), # End tabpanel Two-variable plots
@@ -191,15 +127,15 @@ ui <- navbarPage(
              sidebarPanel(
                selectInput(inputId = "x_multi", 
                            label = "X-axis:", 
-                           choices = names(diabetes), 
+                           choices = NULL, 
                            selected = "Age"), # End input 1
                selectInput(inputId = "y_multi", 
                            label = "Y-axis:",
-                           choices = names(diabetes), 
+                           choices = NULL, 
                            selected = "Pregnancies"), # End input 2
                selectInput(inputId = "third_var", 
                            label =  "Third variable:", 
-                           choices = names(diabetes), 
+                           choices = NULL, 
                            selected = "Insulin"), # End input 3
                colourInput(inputId = "color3",
                            label = "Choose bubble color:",
@@ -209,13 +145,14 @@ ui <- navbarPage(
                             choices = c("Bubble Chart", "Heatmap"),
                             selected = "Bubble Chart") # End radio button
              ), # End sidebarPanel
-             mainPanel(h3("Visualizing multiple variables"),
-                       HTML(r"(
-                              <p>This section is designed for more complex visualizations. 
-                              The Bubble charts allow you to compare three numerical variables at once by encoding them into the x-axis, y-axis, and bubble size.
-                              Whereas the heatmaps can be used to visualize relationships between two variables while incorporating a third variable as a color gradient.</p>
-                         )"), # End HTML
-                       plotOutput("multi_var_plot"))
+             mainPanel(
+               h3("Visualizing multiple variables"),
+               p("This section is designed for more complex visualizations."),
+               p("Bubble charts allow you to compare three numerical variables at once by encoding them into the x-axis, y-axis, and bubble size."),
+               p("Heatmaps can be used to visualize relationships between two variables while incorporating a third variable as a color gradient."),
+               p("Correlation plots show the relationships between all numerical variables in your dataset."),
+               plotOutput("multi_var_plot", height = "500px")
+             ) # End mainPanel
            )# End sidebarlayout
   ) # End tabpanel Multi-variable plots
 ) # End of navpage()
